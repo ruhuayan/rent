03db25c6267f1712afd8c7f75a8263787bd44909
richyan <ruhuayan@gmail.com>
2019-08-18 23:27:55 -0400

    add header

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatButtonModule,
+  // MatTableModule,
+  MatProgressBarModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule
+    // MatTableModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule
+    // MatTableModule,
+  ]
+})
+export class MaterialModule {}
